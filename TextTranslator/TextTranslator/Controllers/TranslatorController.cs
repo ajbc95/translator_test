998ba4fa0bd16e25e25b48453bc85f0f9330bf6b
Alejandro Box <alejandro.box@plainconcepts.com>
2025-09-14 19:18:49 +0200

    refactor: minor change in endpoints signature

--- TextTranslator/TextTranslator/Controllers/TranslatorController.cs
+++ TextTranslator/TextTranslator/Controllers/TranslatorController.cs
@@ -5,15 +5,15 @@ using TextTranslator.Service.Model;
 namespace TextTranslator.Api.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("[controller]/Translate")]
 public class TranslatorController(ITranslatorService translatorService) : ControllerBase
 {
-    [HttpPost("Translate")]
+    [HttpPost()]
     [ProducesResponseType(typeof(Guid), StatusCodes.Status202Accepted)]
     public IActionResult TranslateText([FromBody] string originalText) 
         => Accepted(translatorService.TranslateJob(originalText));
 
-    [HttpGet("Result/{workId}")]
+    [HttpGet("{workId}/Result")]
     [ProducesResponseType(typeof(TranslationResult), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetTranslationResult(Guid workId)
